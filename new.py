717fec74d129af7483f03a8b75a2aa7d3d7daead
FilkaYaroslavsky <ilya19xx2014@gmail.com>
2021-07-25 03:18:24 +0300

    parcer

--- new.py
+++ new.py
@@ -1,4 +1,22 @@
-print('Hello World')
-print("второе изменение")
-for el in range(100):
-    print(el)
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        parser = 'html.parser'
+        sp = BeautifulSoup(html, parser)
+        for tag in sp.find_all('a'):
+            url = tag.get('href')
+            if url is None:
+                continue
+            print('\n' +url)
+
+
+news = 'https://news.google.ru/'
+Scraper(news).scrape()
